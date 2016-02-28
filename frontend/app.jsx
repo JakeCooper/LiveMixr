c73a967bd1e5c37bef1683d551c018dfe0daf263
Ben Hawker <ben@hawker.cc>
2016-02-27 19:42:23 -0800

    Fix primary rendering

--- frontend/app.jsx
+++ frontend/app.jsx
@@ -1,192 +1,192 @@
 var Oauth = React.createClass({
-	getInitialState: function () {
-		return {
-			RedirectURL: "http://livemixr.azurewebsites.net/",
-			ClientID: "870671781604-65ndlh54fkgpjsufkq2pmsn6rm3bvr8p.apps.googleusercontent.com",
-			CookiePolicy: "single_host_origin",
-			RequestVisibleActions: "http://schema.org/AddAction",
-			Scope: "https://www.googleapis.com/auth/plus.profile.emails.read",
-			ProfileName: null,
-			ProfileImageUrl: null
-		};
-	},
+    getInitialState: function () {
+        return {
+            RedirectURL: "http://livemixr.azurewebsites.net/",
+            ClientID: "870671781604-65ndlh54fkgpjsufkq2pmsn6rm3bvr8p.apps.googleusercontent.com",
+            CookiePolicy: "single_host_origin",
+            RequestVisibleActions: "http://schema.org/AddAction",
+            Scope: "https://www.googleapis.com/auth/plus.profile.emails.read",
+            ProfileName: null,
+            ProfileImageUrl: null
+        };
+    },
 
-	onLogin: function() {
-		gapi.client.setApiKey("AIzaSyD4f3kc9MA9G4OU1z6zbeaGUOW5fjtt_5E");
+    onLogin: function () {
+        gapi.client.setApiKey("AIzaSyD4f3kc9MA9G4OU1z6zbeaGUOW5fjtt_5E");
 
-		var SigninData = { 
-			'client_id': this.state.ClientID,
-			'cookiepolicy': this.state.CookiePolicy,
-			'requestvisibleactions': this.state.RequestVisibleActions,
-			'scope': this.state.Scope
-		};
+        var SigninData = {
+            'client_id': this.state.ClientID,
+            'cookiepolicy': this.state.CookiePolicy,
+            'requestvisibleactions': this.state.RequestVisibleActions,
+            'scope': this.state.Scope
+        };
 
-		// lol good luck
-		gapi.auth.authorize( 
-			SigninData,
-			this.onAuthCallback
-		);
-	},
-	onAuthCallback: function(AuthResult) {
-		if (AuthResult && !AuthResult.error) {
+        // lol good luck
+        gapi.auth.authorize(
+            SigninData,
+            this.onAuthCallback
+        );
+    },
+    onAuthCallback: function (AuthResult) {
+        if (AuthResult && !AuthResult.error) {
 
-			var that = this;
+            var that = this;
 
-			this.loadProfileInfo(function() {
+            this.loadProfileInfo(function () {
 
-				React.render(
-					<CommentBox profileName={that.state.ProfileName} profileUrl={that.state.ProfileImageUrl}/>,
-					document.getElementById('content')
-				);
-			});
-			
-		} else {
-			this.setState({content:"Auth Failed!"});
-		}
-	},
-	loadProfileInfo: function(callback) {
+                React.render(
+                    <CommentBox profileName={that.state.ProfileName} profileUrl={that.state.ProfileImageUrl}/>,
+                    document.getElementById('content')
+                );
+            });
 
-		var that = this;
+        } else {
+            this.setState({content: "Auth Failed!"});
+        }
+    },
+    loadProfileInfo: function (callback) {
 
-		gapi.client.load('plus', 'v1').then(function() {
-			var request = gapi.client.plus.people.get({
-				'userId': 'me'
-			});
+        var that = this;
 
-			request.then(function(resp) {
+        gapi.client.load('plus', 'v1').then(function () {
+            var request = gapi.client.plus.people.get({
+                'userId': 'me'
+            });
 
-				that.state.ProfileName = resp.result.displayName;
-				that.state.ProfileImageUrl = resp.result.image.url;
+            request.then(function (resp) {
 
-				callback();
+                that.state.ProfileName = resp.result.displayName;
+                that.state.ProfileImageUrl = resp.result.image.url;
 
-			}, function(reason) {
-				console.log('Error: ' + reason.result.error.message);
-			});
-		});
-	},
-	render: function() {
-		return (
-				<div>
-					<div className="login-header">LiveMixr</div>
-					<button className="auth-button" type="submit" onClick={this.onLogin}>Login</button>
-					<div>{this.state.content}</div>
-				</div>
-			)
-	}
+                callback();
+
+            }, function (reason) {
+                console.log('Error: ' + reason.result.error.message);
+            });
+        });
+    },
+    render: function () {
+        return (
+            <div>
+                <div className="login-header">LiveMixr</div>
+                <button className="auth-button" type="submit" onClick={this.onLogin}>Login</button>
+                <div>{this.state.content}</div>
+            </div>
+        )
+    }
 });
 
 var CommentBox = React.createClass({
-	getInitialState: function () {
-		return {
-			comments: null
-		};
-	},
-	componentDidMount: function () {
-		var that = this;
-		this.socket = io();
-		this.socket.on('comments', function (comments) {
-			that.setState({ comments: comments });
-		});
-		this.socket.emit('fetchComments');
-	},
-	submitComment: function (comment, callback) {
-		this.socket.emit('newComment', comment, function (err) {
-			if (err)
-				return console.error('New comment error:', err);
-			callback();
-		});
-	},
-	render: function() {
-		return (
-			<div className="commentBox">
-				<h2>Hello {this.props.profileName}</h2>
-				<img src={this.props.profileUrl}/>
-				<h3>Comments:</h3>
-				<CommentList comments={this.state.comments}/>
-				<CommentForm submitComment={this.submitComment}/>
-			</div>
-		);
-	}
+    getInitialState: function () {
+        return {
+            comments: null
+        };
+    },
+    componentDidMount: function () {
+        var that = this;
+        this.socket = io();
+        this.socket.on('comments', function (comments) {
+            that.setState({comments: comments});
+        });
+        this.socket.emit('fetchComments');
+    },
+    submitComment: function (comment, callback) {
+        this.socket.emit('newComment', comment, function (err) {
+            if (err)
+                return console.error('New comment error:', err);
+            callback();
+        });
+    },
+    render: function () {
+        return (
+            <div className="commentBox">
+                <h2>Hello {this.props.profileName}</h2>
+                <img src={this.props.profileUrl}/>
+                <h3>Comments:</h3>
+                <CommentList comments={this.state.comments}/>
+                <CommentForm submitComment={this.submitComment}/>
+            </div>
+        );
+    }
 });
 
 var CommentList = React.createClass({
-	render: function () {
-		var Comments = (<div>Loading comments...</div>);
-		if (this.props.comments) {
-			Comments = this.props.comments.map(function (comment) {
-				return (<Comment comment={comment} />);
-			});
-		}
-		return (
-			<div className="commentList">
-				{Comments}
-			</div>
-		);
-	}
+    render: function () {
+        var Comments = (<div>Loading comments...</div>);
+        if (this.props.comments) {
+            Comments = this.props.comments.map(function (comment) {
+                return (<Comment comment={comment}/>);
+            });
+        }
+        return (
+            <div className="commentList">
+                {Comments}
+            </div>
+        );
+    }
 });
 var Comment = React.createClass({
-	render: function () {
-		return (
-			<div className="comment">
-				<span className="author">{this.props.comment.author}</span> said:<br/>
-				<div className="body">{this.props.comment.text}</div>
-			</div>
-		);
-	}
+    render: function () {
+        return (
+            <div className="comment">
+                <span className="author">{this.props.comment.author}</span> said:<br/>
+                <div className="body">{this.props.comment.text}</div>
+            </div>
+        );
+    }
 });
 var CommentForm = React.createClass({
-	handleSubmit: function (e) {
-		e.preventDefault();
-		var that = this;
-		var author = this.refs.author.getDOMNode().value;
-		var text = this.refs.text.getDOMNode().value;
-		var comment = { author: author, text: text };
-		var submitButton = this.refs.submitButton.getDOMNode();
-		submitButton.innerHTML = 'Posting comment...';
-		submitButton.setAttribute('disabled', 'disabled');
-		this.props.submitComment(comment, function (err) {
-			that.refs.author.getDOMNode().value = '';
-			that.refs.text.getDOMNode().value = '';
-			submitButton.innerHTML = 'Post comment';
-			submitButton.removeAttribute('disabled');
-		});
-	},
-	render: function () {
-		return (
-			<div>
-				<form className="commentForm" onSubmit={this.handleSubmit}>
-					<input type="text" name="author" ref="author" placeholder="Name" required /><br/>
-					<textarea name="text" ref="text" placeholder="Comment" required></textarea><br/>
-					<button type="submit" ref="submitButton">Post comment</button>
-				</form>
-			</div>
-		);
-	}
+    handleSubmit: function (e) {
+        e.preventDefault();
+        var that = this;
+        var author = this.refs.author.getDOMNode().value;
+        var text = this.refs.text.getDOMNode().value;
+        var comment = {author: author, text: text};
+        var submitButton = this.refs.submitButton.getDOMNode();
+        submitButton.innerHTML = 'Posting comment...';
+        submitButton.setAttribute('disabled', 'disabled');
+        this.props.submitComment(comment, function (err) {
+            that.refs.author.getDOMNode().value = '';
+            that.refs.text.getDOMNode().value = '';
+            submitButton.innerHTML = 'Post comment';
+            submitButton.removeAttribute('disabled');
+        });
+    },
+    render: function () {
+        return (
+            <div>
+                <form className="commentForm" onSubmit={this.handleSubmit}>
+                    <input type="text" name="author" ref="author" placeholder="Name" required/><br/>
+                    <textarea name="text" ref="text" placeholder="Comment" required></textarea><br/>
+                    <button type="submit" ref="submitButton">Post comment</button>
+                </form>
+            </div>
+        );
+    }
 });
 
 var Navbar = React.createClass({
-	render: function() {
-		return (
-			<div className="navbar navbar-default">
-				I am a navbar
-			</div>
-		)
-	}
+    render: function () {
+        return (
+            <div className="navbar navbar-default">
+                I am a navbar
+            </div>
+        )
+    }
 });
 
 var MainPage = React.createClass({
-	render: function() {
-		return (
-			<div>
-				<Navbar/>
-				<Oauth/>
-			</div>
-		)
-	}
+    render: function () {
+        return (
+            <div>
+                <Navbar/>
+                <Oauth/>
+            </div>
+        )
+    }
 });
 
 React.render(
-	<MainPage/>,
-	document.getElementById('content')
+    <MainPage/>,
+    document.body
 );
