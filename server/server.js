5aa0c2b53f8ca65f9eed9e01388faee2db7d94a8
Jake <jake.elijah.cooper@gmail.com>
2016-02-27 16:30:50 -0800

    Added first servable file

--- server/server.js
+++ server/server.js
@@ -8,12 +8,24 @@ var port = normalizePort(process.env.PORT || '8080');
 var staticPath = path.resolve(__dirname, '/public');
 app.use(express.static(staticPath));
 
+var FrontendPath = __dirname + "/../frontend";
+var StaticPath = __dirname + "/../static";
+
+app.set('view engine', 'html');
+
 app.listen(port, function() {
   console.log('New LiveMixr server on port ' + port);
 });
 
 app.get('/', function(req,res) {
-	res.send("Hello World")
+	
+	// For static files
+	app.use(express.static(path.resolve(StaticPath)));
+
+	// For dynamic js/css
+	app.use(express.static(path.resolve(FrontendPath)));
+	
+	res.sendFile(path.resolve(__dirname + '/../frontend/index.html'))
 })
 
 function normalizePort(val) {
